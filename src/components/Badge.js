17974470fa3f97fd4f56096627bf20711c6f4d67
Luis Mendoza <lmendoza.mg@gmail.com>
2021-03-10 10:41:37 +0100

    css imported and classNames added to the badge file

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,30 @@
 import React from 'react'
+
+import "./styles/Badge.css"
 import PageLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
     render() {
+        const {name, model, avatar, unit_type, manufacturer} = this.props
+
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={PageLogo} alt="Logo de la página" />
                 </div>
 
-                <div>
-                    <img src="" alt="Avatar"/>
-                    <h1>Luis <br/> Mendoza</h1>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src={avatar} alt="Avatar"/>
+                    <h1>{name} <br/> {model}</h1>
                 </div>
 
-                <div>
-                    <p>Systems Engineer</p>
-                    <p>@lmendev</p>
+                <div className="Badge__section-info">
+                    <h3>{unit_type}</h3>
+                    <div>{manufacturer}</div>
+                </div>
+                
+                <div className="Badge__footer">
+                    @lmendev
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const container = document.getElementById('app');
-
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
-*/
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
 
 import Badge from './components/Badge'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(
+    <Badge 
+        name="Ingram" 
+        model="AV-98"
+        avatar="https:/patlabor-api.herokuapp.com/images/labors/1.jpg"
+        unit_type="Police Patrol Labor" 
+        manufacturer="Shinohara Heavy Industries, Hachioji Factory" 
+    />, 
+    container)
 
