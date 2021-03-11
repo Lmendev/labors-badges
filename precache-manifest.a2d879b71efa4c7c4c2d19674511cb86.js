70290a0a12064176c409308feb7559a3b4ed1bbd
Luis Mendoza <lmendoza.mg@gmail.com>
2021-03-11 10:00:26 +0100

    Updates

---
+++ precache-manifest.a2d879b71efa4c7c4c2d19674511cb86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "388e57cf96c81706606a",
+    "url": "/labors-badges/static/css/main.d6a2055c.chunk.css"
+  },
+  {
+    "revision": "388e57cf96c81706606a",
+    "url": "/labors-badges/static/js/main.388e57cf.chunk.js"
+  },
+  {
+    "revision": "579adeda8f2c6463d9c3",
+    "url": "/labors-badges/static/js/runtime~main.579adeda.js"
+  },
+  {
+    "revision": "9dab74629b0546992ffd",
+    "url": "/labors-badges/static/css/2.ab182b0d.chunk.css"
+  },
+  {
+    "revision": "9dab74629b0546992ffd",
+    "url": "/labors-badges/static/js/2.9dab7462.chunk.js"
+  },
+  {
+    "revision": "fe5b3473f300e0c98d483d8b4a7f2e64",
+    "url": "/labors-badges/static/media/badge-header.fe5b3473.svg"
+  },
+  {
+    "revision": "1b7117bdf31c8b69056dcfdf7d6911ec",
+    "url": "/labors-badges/index.html"
+  }
+];
